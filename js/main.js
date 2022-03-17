0da74607cc851863af902b35447f040f06fb95ac
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-17 20:04:48 +0500

    second change

--- js/main.js
+++ js/main.js
@@ -26,79 +26,115 @@ let arr = [
 ]
 
 let elList = document.getElementById('mainList');
-let zero = 0;
+const tax = document.getElementById('tax');
+const total = document.getElementById('total');
+const subtotal = document.getElementById('subtotal');
+
 
 for (let i = 0; i < arr.length; i++){
     let element = document.createElement('li');
+    element.className = 'food-item animate__animated animate__bounceIn'
       element.innerHTML = `
-      <li class="food-item">
       <img class="food-img" src="${arr[i].img}" alt="pizza">
       <div class="ms-3">
       <p class="food-name m-0">${arr[i].text}</p>
       <p class="food-price mt-2">$${arr[i].price}</p>
-      <button class="food-btn">Add to Card</button>
+      <button class="food-btn" onclick = 'addItem(${arr[i].id})'>Add to Card</button>
     </div>
-      </li>
       `;
     elList.append(element);
 }
 
-
 let secondList = document.getElementById('secondList');
-const AddBtn = document.querySelectorAll('.food-btn');
-const subtotal = document.getElementById('subtotal');
-const tax = document.getElementById('tax');
-const total = document.getElementById('total');
+
+let sub = 0;
+let taxx = 0;
+let totalsum = 0;
+let newPizzaArr = [];
+
+function addItem(id){
+  for (let i = 0; i < arr.length; i++){
+    if (id == arr[i].id){
+      newPizzaArr.push(arr[i]);
+
+    }
+}
+  for (let i = 0; i < newPizzaArr.length; i++){
+    if (i == newPizzaArr.length-1){
+        let element = document.createElement('li');
+        element.className = 'food-item animate__animated animate__bounceIn'
+        element.innerHTML = `
+        <img class="food-img" src="${newPizzaArr[i].img}" alt="pizza">
+        <div class="ms-3">
+        <p class="food-name m-0">${newPizzaArr[i].text}</p>
+        <p class="food-price mt-2">$${newPizzaArr[i].price}</p>
+        <span class="minus del remove" onclick='removeItem(${i})'><i class='bx bx-minus' style='color:#ffffff'></i></span>
+      </div>
+        `;
+  
+        sub += newPizzaArr[i].price;
+        taxx = sub * 10 / 100;
+        totalsum = sub + taxx;
+        subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
+        tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
+        total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
 
 
-for (let i = 0; i < AddBtn.length; i++){
-  AddBtn[i].addEventListener('click', () =>{
-    let element = document.createElement('li');
-    element.innerHTML = `
-    <li class="food-item animate__animated animate__bounceIn">
-    <img class="food-img" src="${arr[i].img}" alt="pizza">
-    <div class="ms-3">
-    <p class="food-name m-0">${arr[i].text}</p>
-    <p class="food-price mt-2">$${arr[i].price}</p>
-    <span class="minus del"><i class='bx bx-minus' style='color:#ffffff'></i></span>
-  </div>
-    </li>
-    `;
-  secondList.append(element);
-
-
+        secondList.append(element);
+    }
+}
+}
 
   // REMOVING ITEMS
+ 
 
 
- let removeBtn = document.querySelectorAll('.del');
+function removeItem(index){
+  let newArrRemove = [];
 
-  for (let i = 0; i < removeBtn.length; i++){
-    removeBtn[i].addEventListener('click', (e) =>{
-      e.target.parentNode.parentNode.parentNode.remove()
-    })
+    for (let i = 0; i < newPizzaArr.length; i++){
+      if (index != i){
+       newArrRemove.push(newPizzaArr[i]);
+      }
+    }
+
+    newPizzaArr = newArrRemove;
+    
+    secondList.innerHTML = '';
+    sub = 0;
+    taxx = 0;
+    totalsum = 0;
+    
+    
+    for (let i = 0; i < newPizzaArr.length; i++){
+      
+      let element = document.createElement('li');
+      element.className = 'food-item ';
+      element.innerHTML = `
+      <img class="food-img" src="${newPizzaArr[i].img}" alt="pizza">
+      <div class="ms-3">
+      <p class="food-name m-0">${newPizzaArr[i].text}</p>
+      <p class="food-price mt-2">$${newPizzaArr[i].price}</p>
+      <span class="minus del remove" onclick="removeItem(${i})"><i class='bx bx-minus' style='color:#ffffff'></i></span>
+    </div>
+      `;
+
+     sub += newPizzaArr[i].price;
+     taxx = sub * 10 / 100;
+     totalsum = sub + taxx;
+    
+     subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
+     tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
+     total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
+
+     secondList.appendChild(element);
+    }
+    if (newPizzaArr.length == 0){
+      sub = 0;
+      taxx = 0;
+      subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
+      tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
+      total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
+    }
   }
 
-  //  ADDING TOTAL SUM 
-
-  let sum = zero += arr[i].price;
-  subtotal.innerHTML = `<p> Subtotal: $ ${sum.toFixed(2)}`;
-  let tax1 = sum * 10 / 100;
-  tax.innerHTML = `<p> Tax: $ ${tax1.toFixed(2)} </p>`;
-  let totalsum = sum + tax1;
-  total.innerHTML = `<p> Total: $ ${totalsum.toFixed(2)} </p>`;
-
-  for (let i = 0; i < removeBtn.length; i++){
-    removeBtn[i].addEventListener('click', (e) =>{
-  
-      sum -= arr[i].price;
-      subtotal.innerHTML = `<p> Subtotal: $ ${sum.toFixed(2)}`;
-      let tax1 =  sum * 10 / 100;
-      tax.innerHTML = `<p> Tax: $ ${tax1.toFixed(2)} </p>`;
-      let totalsum = sum + tax1;
-      total.innerHTML = `<p> Total: $ ${totalsum.toFixed(2)} </p>`;
-  
-    })
-  }
-})
-}
