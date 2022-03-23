5ea838af851185118648b1fdaa1098d84298449f
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-23 23:37:42 +0500

    main change

--- js/main.js
+++ js/main.js
@@ -1,140 +1,110 @@
-let arr = [
-   {
-    img: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza1.png',
-    text: 'Peperoni',
-    price: 1,
-    id: 1
-  },
-   {
-    img: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza2.jpg',
-    text: 'Cheesy',
-    price: 2,
-    id: 2
-  },
-  {
-    img: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg',
-    text: 'Margarita',
-    price: 3,
-    id: 3
-  },
-  {
-    img: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg',
-    text: 'Margarita',
-    price: 4,
-    id: 4
-  }
-]
-
-let elList = document.getElementById('mainList');
-const tax = document.getElementById('tax');
-const total = document.getElementById('total');
-const subtotal = document.getElementById('subtotal');
+const menuList = document.getElementById("mainList");
+const secondList = document.getElementById("secondList");
+const subtotal = document.getElementById("subtotal");
+const elTax = document.getElementById("tax");
+const elTotal = document.getElementById("total");
 
 
-for (let i = 0; i < arr.length; i++){
-    let element = document.createElement('li');
-    element.className = 'food-item animate__animated animate__bounceIn'
-      element.innerHTML = `
-      <img class="food-img" src="${arr[i].img}" alt="pizza">
+let newArr = [];
+let total = 0;
+let tax = 0;
+let sum = 0;
+
+arr.forEach((item) => {
+  let li = document.createElement("li");
+  li.className = "food-item";
+  li.innerHTML = `
+  <img class="food-img" src=${item.imgUrl} alt="pizza" />
+  <div class=" ms-3">
+    <p class="food-name">${item.name}</p>
+    <span class="food-price">$${item.price}</span>
+    <button class="food-btn" onclick="addItemArray(${item.id})">
+      Add to Cart
+    </button>
+  </div>
+  `;
+
+  menuList.appendChild(li);
+});
+
+function addItemArray(listId) {
+  newArr.push(arr.filter((item) => item.id === listId)[0]);
+  addCart(newArr);
+}
+
+function addCart(cartPizzaArray) {
+  let arr = cartPizzaArray;
+  let topArr = [];
+
+  arr.forEach(() => {
+    if (arr != "") topArr.push(arr[0]);
+    arr = arr.filter((el) => {
+      return arr[0].id != el.id;
+    });
+  });
+
+  let li = 0;
+  let listArr = [];
+  total = 0;
+  tax = 0;
+  sum = 0;
+
+  topArr.forEach((item) => {
+    let count = cartPizzaArray.filter((element) => {
+      return element.id == item.id;
+    });
+    li = `
+    <li class="food-item">
+      <img class="food-img" src=${item.imgUrl} alt="pizza" />
       <div class="ms-3">
-      <p class="food-name m-0">${arr[i].text}</p>
-      <p class="food-price mt-2">$${arr[i].price}</p>
-      <button class="food-btn" onclick = 'addItem(${arr[i].id})'>Add to Card</button>
-    </div>
-      `;
-    elList.append(element);
-}
-
-let secondList = document.getElementById('secondList');
-
-let sub = 0;
-let taxx = 0;
-let totalsum = 0;
-let newPizzaArr = [];
-
-function addItem(id){
-  for (let i = 0; i < arr.length; i++){
-    if (id == arr[i].id){
-      newPizzaArr.push(arr[i]);
-
-    }
-}
-  for (let i = 0; i < newPizzaArr.length; i++){
-    if (i == newPizzaArr.length-1){
-        let element = document.createElement('li');
-        element.className = 'food-item animate__animated animate__bounceIn'
-        element.innerHTML = `
-        <img class="food-img" src="${newPizzaArr[i].img}" alt="pizza">
-        <div class="ms-3">
-        <p class="food-name m-0">${newPizzaArr[i].text}</p>
-        <p class="food-price mt-2">$${newPizzaArr[i].price}</p>
-        <span class="minus del remove" onclick='removeItem(${i})'><i class='bx bx-minus' style='color:#ffffff'></i></span>
+        <p class="food-name">${item.name}</p>
+        <span id="total" class="count">
+          ${count.length}
+        </span>
+        <span class="food-price">$${item.price}</span>
+        <div class="btn-group">
+          <span class="minus plus" onclick="addItemArray(${item.id})">+</span>
+          <span class="minus" onclick="remove(${item.id})"><i class='bx bx-minus' style='color:#ffffff'  ></i></span>
+        </div>
       </div>
-        `;
+    </li>
+    `;
+
+    listArr.push(li);
+    secondList.innerHTML = listArr.join("");
+  });
+
+  cartPizzaArray.forEach((item) => {
+    total += item.price;
+    tax = total * 10 / 100;
+    sum = total+tax;
+  });
+
+  subtotal.innerHTML = `<p>Subtotal: $${total.toFixed(2)} </p>`;
+  elTax.innerHTML = `<p>Tax: $${tax.toFixed(2)} </p>`;
+  elTotal.innerHTML = `<p>Total: $${sum.toFixed(2)} </p>`;
+
   
-        sub += newPizzaArr[i].price;
-        taxx = sub * 10 / 100;
-        totalsum = sub + taxx;
-        subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
-        tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
-        total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
-
-
-        secondList.append(element);
-    }
-}
 }
 
-  // REMOVING ITEMS
- 
+function remove(elId) {
+  let count = 0;
+  let a = [];
 
-
-function removeItem(index){
-  let newArrRemove = [];
-
-    for (let i = 0; i < newPizzaArr.length; i++){
-      if (index != i){
-       newArrRemove.push(newPizzaArr[i]);
-      }
+  newArr.forEach((element) => {
+    if (element.id === elId && count === 0) {
+      count++;
+    } else {
+      a.push(element);
     }
+  });
 
-    newPizzaArr = newArrRemove;
-    
-    secondList.innerHTML = '';
-    sub = 0;
-    taxx = 0;
-    totalsum = 0;
-    
-    
-    for (let i = 0; i < newPizzaArr.length; i++){
-      
-      let element = document.createElement('li');
-      element.className = 'food-item ';
-      element.innerHTML = `
-      <img class="food-img" src="${newPizzaArr[i].img}" alt="pizza">
-      <div class="ms-3">
-      <p class="food-name m-0">${newPizzaArr[i].text}</p>
-      <p class="food-price mt-2">$${newPizzaArr[i].price}</p>
-      <span class="minus del remove" onclick="removeItem(${i})"><i class='bx bx-minus' style='color:#ffffff'></i></span>
-    </div>
-      `;
-
-     sub += newPizzaArr[i].price;
-     taxx = sub * 10 / 100;
-     totalsum = sub + taxx;
-    
-     subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
-     tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
-     total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
-
-     secondList.appendChild(element);
-    }
-    if (newPizzaArr.length == 0){
-      sub = 0;
-      taxx = 0;
-      subtotal.innerHTML = `<p> Subtotal: $${sub.toFixed(2)} </p>`;
-      tax.innerHTML = `<p> Tax: $${taxx.toFixed(2)} </p>`;
-      total.innerHTML = `<p> Total: $${totalsum.toFixed(2)} </p>`;
-    }
+  newArr = a;
+  if (newArr.length === 0) {
+    secondList.innerHTML = "";
   }
+  addCart(newArr);
+}
+
+
 
---
+++ js/object.js
@@ -0,0 +1,26 @@
+let arr = [
+  {
+   imgUrl: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza1.png',
+   name: 'Peperoni',
+   price: 1,
+   id: 1
+ },
+  {
+   imgUrl: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza2.jpg',
+   name: 'Cheesy',
+   price: 2,
+   id: 2
+ },
+ {
+  imgUrl: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg',
+  name: 'Margarita',
+  price: 3,
+  id: 3
+ },
+ {
+  imgUrl: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg',
+  name: 'Margarita',
+  price: 4,
+  id: 4
+ }
+]
